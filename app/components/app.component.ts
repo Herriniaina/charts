22e23211101f0d0ae545fd104bb5e2f92362428c
Herriniaina <plano.heriniaina@shasama.com>
2016-06-06 16:11:03 +0300

    fix indentation

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -4,19 +4,19 @@ import { CORE_DIRECTIVES } from '@angular/common';
 import {Router,Routes,ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
-    selector: 'my-app',
-    directives:[ChartComponent,CORE_DIRECTIVES,ROUTER_DIRECTIVES],
-    template: `
-    	<h1>Loocho Charts</h1>
-    	<div class="dropdown">
-    	  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Type of the Graph
- 			<span class="caret"></span></button>
-			<ul class="dropdown-menu">
-			  <li  *ngFor="let cType of charTypes"><a [routerLink]="['/', cType.name]">{{ cType.value }}</a></li>
-			</ul>
-		</div>
-    	<my-chart></my-chart>
-    	<router-outlet></router-outlet>
+  selector: 'my-app',
+  directives:[ChartComponent,CORE_DIRECTIVES,ROUTER_DIRECTIVES],
+  template: `
+   	<h1>Loocho Charts</h1>
+    <div class="dropdown">
+      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Type of the Graph
+ 	    <span class="caret"></span></button>
+	    <ul class="dropdown-menu">
+	      <li  *ngFor="let cType of charTypes"><a [routerLink]="['/', cType.name]">{{ cType.value }}</a></li>
+	    </ul>
+	  </div>
+      <my-chart></my-chart>
+      <router-outlet></router-outlet>
     `
 })
 
@@ -25,36 +25,37 @@ import {Router,Routes,ROUTER_DIRECTIVES} from '@angular/router';
 ])
 
 export class AppComponent { 
-    public charTypes=[
-	    {
-	      'name': 'BarChart',
-	      'value': 'Bar Chart'
-	    },
-	   	{
-	      'name': 'BubbleChart',
-	      'value': 'Bubble Chart'
-	    },
-	    {
-	      'name': 'ColumnChart',
-	      'value': 'Column Chart'
-	    },
-	   	{
-	      'name': 'LineChart',
-	      'value': 'Line Chart'
-	    },
-	    {
-	      'name': 'PieChart',
-	      'value': 'Pie Chart'
-	    },
-  	];
-    public columns=[
-	    {
-	      'name': 'contact',
-	      'value': 'Contact'
-	    },
-	   	{
-	      'name': 'meeting',
-	      'value': 'Meeting'
-	    }
-  	];
+  public charTypes=[
+    {
+	  'name': 'BarChart',
+	  'value': 'Bar Chart'
+	},
+	{
+	  'name': 'BubbleChart',
+	  'value': 'Bubble Chart'
+	},
+	{
+	  'name': 'ColumnChart',
+	  'value': 'Column Chart'
+	},
+	{
+	  'name': 'LineChart',
+	  'value': 'Line Chart'
+	},
+	{
+	  'name': 'PieChart',
+	  'value': 'Pie Chart'
+	},
+  ];
+
+  public columns=[
+    {
+	  'name': 'contact',
+	  'value': 'Contact'
+	},
+	{
+	  'name': 'meeting',
+	  'value': 'Meeting'
+	}
+  ];
 }
--- app/services/chart.service.ts
+++ app/services/chart.service.ts
@@ -11,5 +11,4 @@ export class ChartService {
   getOptions(){
     return Promise.resolve(CHARTOPTIONS);
   }
-
 }
